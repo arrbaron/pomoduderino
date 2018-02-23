bf864e6905b88c534278fc7deaffa2720323f254
Roy Baron <baron.roy@gmail.com>
2018-02-23 18:56:32 -0500

    add time logic and display

--- src/containers/Timer.js
+++ src/containers/Timer.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Display from '../components/Display';
 import Controls from '../components/Controls';
 
 class Timer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      minutes: 15,
+      seconds: 0,
+      timeDisplay: ''
+    }
+  }
+  
+  startTimer() {
+    setInterval(() => {
+      if (this.state.seconds <= 0) {
+        this.setState({
+          seconds: 60,
+          minutes: this.state.minutes - 1
+        })
+      }
+      this.setState({ seconds: this.state.seconds - 1});
+      this.formatTime();
+    }, 1000)
+  }
+
+  formatTime() {
+    let seconds = parseInt(this.state.seconds, 10);
+    let minutes = parseInt(this.state.minutes, 10);
+
+    if (seconds < 10) seconds = '0' + seconds;
+    if (minutes < 10) minutes = '0' + minutes;
+    
+    this.setState({ timeDisplay: `${minutes} : ${seconds}`});
+  }
+
+  componentDidMount() {
+    this.startTimer();
+  }
+  
   render() {
     return (
       <section className='timer'>
-        <Display />
+        <Display time={this.state.timeDisplay} />
         <Controls />
       </section>
     );
   }
 }
 
-export default Timer;
+const mapStateToProps = state => ({
+  time: state.reducer.time
+});
+
+export default connect(mapStateToProps)(Timer);
