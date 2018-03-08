00899780501228375a665911674d02977fba6a99
Roy Baron <baron.roy@gmail.com>
2018-03-08 17:55:57 -0500

    add footer styling

--- src/containers/Timer.js
+++ src/containers/Timer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Controls from '../components/Controls';
 import Display from '../components/Display';
+import '../styles/Timer.css';
 
 class Timer extends Component {
   constructor() {
