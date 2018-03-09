3c246a38771833ea0dab5ea94a17c9ffc2b67917
Roy Baron <baron.roy@gmail.com>
2018-03-09 15:46:54 -0500

    disable popup notifications

--- src/containers/Alert.js
+++ src/containers/Alert.js
@@ -15,25 +15,19 @@ class Alert extends Component {
     }
   }
 
-  onMobile() {
-    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-  }
-
   sendAlert(status) {
     let audio = null;
-    let notification = null;
-    if (!this.onMobile()) {
-      if (status === 'working') {
-        notification = new Notification('HERE WE GO', {
-          body: this.props.texts[1]
-        });
-        audio = new Audio(abides);
-      } else if (status === 'resting') {
-        notification = new Notification('Relax, man.', {
-          body: this.props.texts[2]
-        });
-        audio = new Audio(bowling);
-      }
+    // let notification = null;
+    if (status === 'working') {
+      // notification = new Notification('HERE WE GO', {
+      //   body: this.props.texts[1]
+      // });
+      audio = new Audio(abides);
+    } else if (status === 'resting') {
+      // notification = new Notification('Relax, man.', {
+      //   body: this.props.texts[2]
+      // });
+      audio = new Audio(bowling);
     }
     if (audio && notification) audio.play();
   }
@@ -44,12 +38,12 @@ class Alert extends Component {
 }
 
 Alert.propTypes = {
-  texts: PropTypes.arrayOf(PropTypes.string).isRequired,
+  // texts: PropTypes.arrayOf(PropTypes.string).isRequired,
   status: PropTypes.string.isRequired
 };
 
 const mapStateToProps = state => ({
-  texts: state.alertReducer.texts,
+  // texts: state.alertReducer.texts,
   status: state.timerReducer.status
 });
 
